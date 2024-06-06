3eea5f8f55ca049a46a37de646581a6e4b175c53
Rowan Dayton-Oxland <r.daytonoxland@outlook.com>
2024-06-06 16:06:39 +0200

    integrating moon stuff more fully

--- sun_angle.py
+++ sun_angle.py
@@ -1,5 +1,8 @@
-"""For quickly converting location and UTC time to Solar elevation angle"""
+"""For quickly converting location and UTC time to Solar elevation angle
+Rowan Dayton-Oxland for ARCTICS, 6 Jun 2024
+"""
 
+# Libraries
 import astropy.units as u
 from astropy.coordinates import AltAz, EarthLocation, SkyCoord, Angle, get_body
 from astropy.time import Time
@@ -8,50 +11,64 @@ import pandas as pd
 
 
 def get_data(filename):
-
+    """Read csv file to pandas dataframe
+    Input: filename (string)
+    Output: df pandas dataframe """
     return pd.read_csv(filename)
 
 
 def sun_loc(obstime):
-
+    """Get centre of solar disk SkyCoord object at observation time
+    Input: obstime (astopy.time Time object) 
+    Output: astropy.SkyCoord location
+    """
     return SkyCoord(0 * u.arcsec, 0 * u.arcsec, obstime=obstime, observer="earth", frame=frames.Helioprojective)
 
 
 def moon_loc(obstime):
-
+    """ Get moon location from Earth at observation time as SkyCoord object
+    Input: obstime (astopy.time Time object) 
+    Output: astropy.SkyCoord location"""
     return get_body(body="moon", time=obstime)
 
 
-def get_elevation(obstime, latitude, longitude, moon=None):
-    """Default to sun, for moon instead add keyword moon"""
+def get_elevation(obstime, latitude, longitude, moon=None, both=None):
+    """ Default to sun, for moon instead add keyword moon
+    Input: obstime (astropy.time Time object)
+           latitude (float) in degrees
+           longitude (float) in degrees
+    Kwarg: moon (optional argument) switches to moon elevation
+    Output: astropy.coordinates Angle object"""
 
+    # Observers location (hard-coded to be at sea level but this could be changed)
     obsloca = EarthLocation(lat=latitude*u.deg, lon=longitude*u.deg, height=0*u.km)
-    
-    # Define centre of the solar disk
-    if moon:
-        c = moon_loc(obstime)
-    else:
-        c = sun_loc(obstime)
 
     # Define observers frame
     frame_altaz = AltAz(obstime=Time(obstime), location=obsloca)
-    
-    # Transform sun location to observers altaz frame
-    altaz = c.transform_to(frame_altaz)
-    
-    # Return solar elevation angle
-    return Angle(altaz.T.alt)
+
+    # Get location of body in altaz
+    if moon:
+        m = moon_loc(obstime)
+        altaz_moon = m.transform_to(frame_altaz)
+        moon_elevation = Angle(altaz_moon.T.alt)
+        return moon_elevation
+    if both:
+        s = sun_loc(obstime)
+        altaz_sun = s.transform_to(frame_altaz)
+        sun_elevation = Angle(altaz_sun.T.alt)
+        return sun_elevation, moon_elevation
+    else:
+        return sun_elevation
 
 
-
-
-def convert_todataframe(filename, moon=None):
+def convert_todataframe(filename, moon=None, both=None):
+    """Convert a csv file to a pandas dataframe including an angle"""
     
     df = get_data(filename)
 
     els=[]
     for index, row in df.iterrows():
-        el = get_elevation(Time(row['time']), row['lat'], row['lon'], moon=moon)
+        el = get_elevation(Time(row['time']), row['lat'], row['lon'], moon=moon, both=both)
         els.append(el)
 
     df['Angle'] = els
@@ -59,9 +76,9 @@ def convert_todataframe(filename, moon=None):
     return df
 
 
-def convert_csv(filename, tofile=None, moon=None):
+def convert_csv(filename, tofile=None, moon=None, both=None):
 
-    df = convert_todataframe(filename, moon=moon)
+    df = convert_todataframe(filename, moon=moon, both=both)
     if tofile:
         df.to_csv(tofile)
     else:

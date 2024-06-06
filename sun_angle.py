70fb0dd234b2a400a67616e300506e0e1269af58
Rowan Dayton-Oxland <r.daytonoxland@outlook.com>
2024-06-06 12:34:55 +0200

    rename to sun_angle

--- sun_angle.py
+++ sun_angle.py
@@ -4,14 +4,25 @@ import astropy.units as u
 from astropy.coordinates import AltAz, EarthLocation, SkyCoord
 from astropy.time import Time
 from sunpy.coordinates import frames
+import pandas as pd
+
+
+def get_data(filename):
+
+    return pd.read_csv(filename)
+
+
+def sun_loc(obstime):
+
+    return SkyCoord(0 * u.arcsec, 0 * u.arcsec, obstime=obstime, observer="earth", frame=frames.Helioprojective)
+
 
 def get_solel(obstime, latitude, longitude):
 
     obsloca = EarthLocation(lat=latitude*u.deg, lon=longitude*u.deg, height=0*u.km)
     
     # Define centre of the solar disk
-    c = SkyCoord(0 * u.arcsec, 0 * u.arcsec, obstime=obstime,
-             observer="earth", frame=frames.Helioprojective)
+    c = sun_loc(obstime)
 
     # Define observers frame
     frame_altaz = AltAz(obstime=Time(obstime), location=obsloca)
@@ -20,12 +31,26 @@ def get_solel(obstime, latitude, longitude):
     sun_altaz = c.transform_to(frame_altaz)
     
     # Return solar elevation angle
-    return sun_altaz.T.alt
+    return astropy.coordinates.Angle(sun_altaz.T.alt)
+
+
+def convert_file(filename):
+    
+    df = get_data(filename)
+
+    els=[]
+    for index, row in df.iterrows():
+        solel = get_solel(Time(row['time']), row['lat'], row['lon'])
+        els.append(solel)
+
+    df['Sun_angle'] = els
+
+    return df
 
 
 """ Example conversion """
+filename = 'example.csv'
 obstime = "2024-09-21 19:00:00" # UTC
 latitude = 56
 longitude = -5
-
 get_solel(obstime, latitude, longitude)
